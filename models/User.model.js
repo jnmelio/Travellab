2480d7e310544ca44f4ae1290bb81cd2de698981
unknown <julienonnenmacher@hotmail.fr>
2021-04-22 15:00:12 +0200

    first merge

--- models/User.model.js
+++ models/User.model.js
@@ -3,10 +3,21 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
   username: {
-    type: String,
-    // unique: true -> Ideally, should be unique, but its up to you
+    type: String,   
+    unique: true, 
   },
-  password: String,
+  password: {
+    type:String,
+    required:true,
+  },
+  email : {
+    type: String, 
+    required: true
+  },
+  age : Number, 
+  photo: String, 
+  favoriteCountry: String, 
+  favoriteWayOfTraveling: String, 
 });
 
 const User = model("User", userSchema);
